e5a2542363241c1a20286347765d660f6ce8c8b9
winkingfoxy <60498435+winkingfoxy@users.noreply.github.com>
2020-02-06 01:51:17 +0200

    send script fix

---
+++ dynamic/e-send.php
@@ -0,0 +1,36 @@
+<?php
+    //Получаем данные из глобальной переменной $_GET, так как мы передаем данные методом GET
+    $email = $_POST['email']; // Вытаскиваем имя в переменную
+    $message = "Email: $email"; // Формируем сообщение, отправляемое на почту
+    $to = "powerandsoull@gmail.com"; // Задаем получателя письма
+    $from = "noreply"; // От кого пришло письмо
+    $subject = "Заказ с Вашего сайта"; // Задаем тему письма
+    $headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
+    if (mail($to, $subject, $message, $headers)) { // При помощи функции mail, отправляем сообщение, проверяя отправилось оно или нет
+        echo '<center><br /><br /><br /><h1 style="font-size: 25pt;color:#FFFFFF;font-family:Tahoma;"><b>Сообщение успешно отправлено.</b></h1></center>'; // Отправка успешна
+    }
+    else {
+        echo "<p>Что-то пошло не так, как планировалось</p>"; // Письмо не отправилось
+    }
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+
+$mail->Host       = "smtp.gmail.com";
+$mail->SMTPDebug  = 0;
+$mail->SMTPAuth   = true;
+$mail->Port       = 465;
+$mail->Username   = "powerandsoull@gmail.com";
+$mail->Password   = "gigsikpoc12";
+
+$mail->From = 'noreply';
+$mail->Subject = 'Заказ с Вашего сайта';
+$mail->addAddress($to);
+$mail->Body = $message;
+
+if(!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message has been sent successfully";
+}
