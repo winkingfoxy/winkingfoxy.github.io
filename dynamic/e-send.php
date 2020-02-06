d0a6943dcaaacf69dfd442253f12264c797764bb
winkingfoxy <60498435+winkingfoxy@users.noreply.github.com>
2020-02-06 09:17:00 +0200

    Update e-send.php

--- dynamic/e-send.php
+++ dynamic/e-send.php
@@ -4,7 +4,7 @@
     $message = "Email: $email"; // Формируем сообщение, отправляемое на почту
     $to = "powerandsoull@gmail.com"; // Задаем получателя письма
     $from = "noreply"; // От кого пришло письмо
-    $subject = "Заказ с Вашего сайта"; // Задаем тему письма
+    $subject = "subscribtion"; // Задаем тему письма
     $headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
     if (mail($to, $subject, $message, $headers)) { // При помощи функции mail, отправляем сообщение, проверяя отправилось оно или нет
         echo '<center><br /><br /><br /><h1 style="font-size: 25pt;color:#FFFFFF;font-family:Tahoma;"><b>Сообщение успешно отправлено.</b></h1></center>'; // Отправка успешна
@@ -21,11 +21,11 @@ $mail->Host       = "smtp.gmail.com";
 $mail->SMTPDebug  = 0;
 $mail->SMTPAuth   = true;
 $mail->Port       = 465;
-$mail->Username   = "powerandsoull@gmail.com";
-$mail->Password   = "gigsikpoc12";
+$mail->Username   = "your egmail adress here";
+$mail->Password   = "yourgmail password here";
 
 $mail->From = 'noreply';
-$mail->Subject = 'Заказ с Вашего сайта';
+$mail->Subject = 'subscription';
 $mail->addAddress($to);
 $mail->Body = $message;
 
